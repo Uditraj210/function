7fe2d18ab95b3e59fe52d5cb31a7bdd28f4a8648
Uditraj210 <117984433+Uditraj210@users.noreply.github.com>
2025-07-20 19:27:30 +0530

    Create function6.js

---
+++ function6.js
@@ -0,0 +1,8 @@
+function findIndex(arr, val) {
+  return arr.indexOf(val);
+}
+
+console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh"));          
+console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue"));       
+console.log(findIndex(["a", "g", "y", "d"], "d"));                       
+console.log(findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple")); 
